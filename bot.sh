b247594197f26e07b875e1d7f94d449cc0b8707d
TThai <163569473+Triutheis@users.noreply.github.com>
2024-09-21 23:58:08 +0700

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -4,3 +4,24 @@ apt-get install wget -y
 apt-get install proot -y
 apt-get install git -y
 clear
+git clone https://github.com/MFDGaming/ubuntu-in-termux
+ls
+cd ubuntu-in-termux
+ls
+chmod +x ubuntu.sh
+./ubuntu.sh -y
+./startubuntu
+clear
+apt update
+apt upgrade
+apt install sudo
+sudo apt install nodejs
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh
+sudo apt install curl
+sudo apt install git
+sudo apt install wget
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+source ~/.bashrc
+ls
+nvm ls-remote
